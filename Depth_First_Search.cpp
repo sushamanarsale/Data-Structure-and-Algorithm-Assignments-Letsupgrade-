1b1bf7c24688bd07783ebd7a1be0718f2116c5f6
sushamanarsale <88391326+sushamanarsale@users.noreply.github.com>
2023-01-03 21:57:25 +0530

    Depth First Search (DFS) implementation

---
+++ Depth_First_Search.cpp
@@ -0,0 +1,34 @@
+// Depth First Search (DFS) implementation
+
+#include<iostream>
+using namespace std;
+ 
+int visited[7] = {0,0,0,0,0,0,0};
+
+    int a[7][7] = {
+        {0,1,1,1,0,0,0},
+        {1,0,1,0,0,0,0},
+        {1,1,0,1,1,0,0},
+        {1,0,1,0,1,0,0},
+        {0,0,1,1,0,1,1},
+        {0,0,0,0,1,0,0}, 
+        {0,0,0,0,1,0,0} 
+    };
+ 
+void DFS(int i){
+    cout<< i<<"\t";
+    visited[i] = 1;
+    for (int j = 0; j < 7; j++)
+    {
+        if(a[i][j]==1 && !visited[j]){
+            DFS(j);
+        }
+    }
+}
+ 
+int main(){ 
+     
+    DFS(5); 
+    return 0;
+}
+
